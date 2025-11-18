79c6bcc0f88a614cf432049f94414c3cded8b468
fliqqs <dox.is.op@gmail.com>
2025-11-18 03:05:03 -0500

    correct type

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/3D-projection-helper/',
+  base: '/3d-projection-helper/',
   publicDir: '../public',
   server: {
     port: 3000,
